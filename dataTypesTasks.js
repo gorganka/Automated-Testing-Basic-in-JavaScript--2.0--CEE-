a2576df7ba29565135e366f76789d221fefce98e
gorganka <chizhikpizhik90@gmail.com>
2023-01-21 21:27:10 +0200

    dataTypeTasks
    Conditional statements and Data types Practical task:
    
    1. Perform addition of various types (string + boolean, string + number, number + boolean)
    2. Perform multiplication of various types (string * boolean, string * number, number * boolean)
    3. Divide different types (string / boolean, string / number, number / Boolean)
    4. Perform explicit conversion (number, string, boolean)

---
+++ dataTypesTasks.js
@@ -0,0 +1,32 @@
+//Perform addition of various types (string + boolean, string + number, number + boolean)
+let string = "JavaScript";
+let boolean = true;
+let number = 8;
+
+console.log(string + boolean); //string + boolean
+console.log(string + number); // string + number
+console.log(number + boolean); // number + boolean
+
+//Perform multiplication of various types (string * boolean, string * number, number * boolean)
+
+console.log(string * boolean); //string * boolean
+console.log(string * number); // string * number
+console.log(number * boolean); // number * boolean
+
+//Divide different types (string / boolean, string / number, number / Boolean)
+
+console.log(string / boolean); //string / boolean
+console.log(string / number); // string / number
+console.log(number / boolean); // number / Boolean
+
+//Perform explicit conversion (number, string, boolean)
+
+let num2 = 35;//type of is 'number'
+let str2 = String (num2);//explicitly convert a number to a string
+
+let str = '555'; //type of is 'string'
+let num = Number (str);//explicitly convert a string to a number
+
+let bool = true; //type of is 'boolean'
+let num3 = Number (bool); //explicitly convert boolean to a number
+
