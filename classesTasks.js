bdce4036c782067c7e1fe964f566f64484129273
gorganka <chizhikpizhik90@gmail.com>
2023-02-01 20:16:02 +0200

    classesTasks
    Prototype and Inheritance, Classes, Constructor practical task
    
    Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal

---
+++ classesTasks.js
@@ -0,0 +1,12 @@
+//Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
+
+
+let animal = {
+    move: "jump"
+};
+
+let cat = {
+    __proto__: animal
+};
+
+console.log(cat.move);
