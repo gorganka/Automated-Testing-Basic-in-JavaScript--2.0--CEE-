1afbf5e9ce8612830d9d02091c60e2ff384075cc
gorganka <chizhikpizhik90@gmail.com>
2023-02-08 15:51:54 +0200

    try-catch
    Exception handling Practical Task
    
    In a try catch construction, wrap the code: console.log (a), let a = 3. And display an error - ‘let must be declared’ before use. When running 1/0, the error 'cannot be divided by zero'

---
+++ try-catchTask.js
@@ -0,0 +1,17 @@
+//In a try catch construction, wrap the code: console.log (a), let a = 3. And display an error - ‘let must be declared’ before use. When running 1/0, the error 'cannot be divided by zero'
+
+try {
+    console.log(a);
+    let a = 3;
+} catch {
+    console.log('let must be declared before use');
+}
+
+try {
+    let a = 1/0;
+    if (a === Infinity) {
+        throw new Error ('cannot be divided by zero');
+    }
+} catch (err) {
+    console.log (err.message);
+}
